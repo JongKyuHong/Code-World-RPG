ea7e868d4d82c96695e851415d379fbb69cc1c0e
hyunseo <hansx2079@naver.com>
2026-01-02 20:13:03 +0900

    update: main 옮기기 test코드 삭제

--- Project1/Project1/Engine.cpp
+++ Project1/Project1/Engine.cpp
@@ -39,38 +39,38 @@ Renderer& Engine::GetRenderer()
     return renderer;
 }
 
-int main() {
-    Engine engine(160, 50);
-
-    SceneManager::GetInstance().Register("TestScene", [&]() {
-        return std::make_unique<TestScene>();
-        });
-    SceneManager::GetInstance().Register("MainMenu", [&]() {
-        return std::make_unique<MainMenu>();
-        });
-
-    using clock = std::chrono::steady_clock;
-    auto prev = clock::now();
-
-    SceneManager::GetInstance().LoadScene("MainMenu");
-
-    while (engine.IsRunning())
-    {
-        auto now = clock::now();
-        std::chrono::duration<float> delta = now - prev;
-        prev = now;
-        float dt = delta.count();
-
-        engine.Update(dt);
-
-        if (Engine::exitRequested == true) {
-            break;
-        }
-    }
-
-    if (Engine::isMainMenu == true) {
-        std::cout << "Main Menu로 돌아갑니다." << std::endl;
-    }
-
-    return 0;
-}
+//int main() {
+//    Engine engine(160, 50);
+//
+//    SceneManager::GetInstance().Register("TestScene", [&]() {
+//        return std::make_unique<TestScene>();
+//        });
+//    SceneManager::GetInstance().Register("MainMenu", [&]() {
+//        return std::make_unique<MainMenu>();
+//        });
+//
+//    using clock = std::chrono::steady_clock;
+//    auto prev = clock::now();
+//
+//    SceneManager::GetInstance().LoadScene("MainMenu");
+//
+//    while (engine.IsRunning())
+//    {
+//        auto now = clock::now();
+//        std::chrono::duration<float> delta = now - prev;
+//        prev = now;
+//        float dt = delta.count();
+//
+//        engine.Update(dt);
+//
+//        if (Engine::exitRequested == true) {
+//            break;
+//        }
+//    }
+//
+//    if (Engine::isMainMenu == true) {
+//        std::cout << "Main Menu로 돌아갑니다." << std::endl;
+//    }
+//
+//    return 0;
+//}
