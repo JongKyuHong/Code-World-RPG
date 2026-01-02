a6eb3e811f32c4f1f2faa0f08aa6eaadbb5b0c51
heewonlee <lhw4540@naver.com>
2026-01-02 10:36:29 +0900

    fix : Item 수정
    아이템 효과 데이터로 받아오는 형태로 수정
    use  기능 삭제
    getEffect 기능 추가
    getName 기능 구현
    getPrice  기능 구현

--- Project1/Project1/Item.cpp
+++ Project1/Project1/Item.cpp
@@ -1,1 +1,13 @@
-#include "Item.h"
+﻿#include "Item.h"
+
+Item::Item(std::string name,int price, std::vector<Effect> effect) :
+	name(std::move(name)), price(price), effect(std::move(effect)) {
+}
+
+std::string Item::getName() const {
+	return name;
+}
+int Item::getPrice() const {
+	return price;
+}
+const std::vector<Effect>& Item:: getEffect() const { return effect; }
