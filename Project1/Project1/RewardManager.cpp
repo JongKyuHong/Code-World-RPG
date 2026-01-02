ada50653f46c823b4a8cd12f107cdb7a9f4bf763
HongJongKyu <kongi7064@gmail.com>
2026-01-02 19:36:17 +0900

    add : RewardManager헤더 정의

---
+++ Project1/Project1/RewardManager.cpp
@@ -0,0 +1,10 @@
+#include "RewardManager.h"
+#include "BattleResult.h"
+#include "Monster.h"
+
+RewardManager* RewardManager::instance = nullptr;
+
+BattleResult RewardManager::generateRewards(const Monster* monster) {
+
+}
+
