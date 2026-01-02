7ebdb923e136d44c345c56ad10e0a215de4b1376
justis0419-maker <justis0419@gmail.com>
2026-01-02 17:07:44 +0900

    크리티컬 퍼센트,데미지적용
    30%확률 크리티컬 발생
    크리티컬 발생시 데미지 2배 적용

--- Project1/Project1/BattleService.cpp
+++ Project1/Project1/BattleService.cpp
@@ -1,1 +1,35 @@
-#include "BattleService.h"
+#include "BattleService.h" // 내 설계도를 가져옵니다.
+
+
+#include <cstdlib>         // rand() 함수를 위해 필요합니다.
+
+// 1. 크리티컬 여부 결정 (정의)
+// BattleService:: 를 붙여서 "BattleService 소속 함수다"라고 알려줍니다.
+
+bool BattleService::rollCritical()
+{
+    // 0~9 사이의 숫자 중 0, 1, 2가 나오면 크리티컬 (30%)
+    if (rand() % 10 < 3)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// 2. 크리티컬 데미지 적용 (정의)
+int BattleService::applyCriticalMultiplier(int baseDamage, bool isCritical)
+{
+    if (isCritical == true)
+    {
+        // 크리티컬이면 2배로 뻥튀기
+        return baseDamage * 2;
+    }
+    else
+    {
+        // 아니면 원래 데미지 그대로
+        return baseDamage;
+    }
+}
--- Project1/Project1/BattleService.h
+++ Project1/Project1/BattleService.h
@@ -1,13 +1,13 @@
 #pragma once
 
-// 전투에 관련된 계산
+// 전투에 관련된 계산 (설계도)
 class BattleService
 {
 public:
-    // 크리티컬 확률
-    bool rollCritical() {}
+    // 1. 크리티컬 여부 결정 (선언)
+    bool rollCritical();
 
-    // 크리티컬 데미지 적용
-    int applyCriticalMultiplier(int baseDamage, bool isCritical) {};
+    // 2. 크리티컬 데미지 적용 (선언)
+    int applyCriticalMultiplier(int baseDamage, bool isCritical);
 };
 
