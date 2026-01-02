fade8982143c0f01b8aa9bac316c3d984bdcab1b
heewonlee <lhw4540@naver.com>
2026-01-02 19:02:11 +0900

    fix : Item Merge 수정
    fix : Item Merge 수정

--- Project1/Project1/Item.h
+++ Project1/Project1/Item.h
@@ -1,4 +1,5 @@
-﻿#pragma once
+﻿<<<<<<< HEAD
+癤?pragma once
 
 #include "Character.h"
 #include "Types.h"
@@ -15,18 +16,113 @@ public:
 
     virtual ~Item() = default;
 
-    //아이템 이름
+    //?꾩씠???대쫫
     virtual std::string getName() const;
     virtual int getPrice() const;
 
-    //아이템이 “어떤 효과를 내는지” 데이터로 제공
+    //?꾩씠?쒖씠 ?쒖뼱???④낵瑜??대뒗吏???곗씠?곕줈 ?쒓났
     virtual const std::vector<Effect>& getEffect() const;
 
-    //상점, 캐릭터 아이템 포인터 공유 문제 해결 위해 상점용 새 인스턴스 추가
-    // 상점에 있는  아이템을 플레이어 개인 소유의** 독립된 새 아이템으로 복사하기 위해 사용
+    //?곸젏, 罹먮┃???꾩씠???ъ씤??怨듭쑀 臾몄젣 ?닿껐 ?꾪빐 ?곸젏?????몄뒪?댁뒪 異붽?
+    // ?곸젏???덈뒗  ?꾩씠?쒖쓣 ?뚮젅?댁뼱 媛쒖씤 ?뚯쑀??* ?낅┰?????꾩씠?쒖쑝濡?蹂듭궗?섍린 ?꾪빐 ?ъ슜
     virtual Item* clone() const = 0;
     bool isHealItem() const;
     bool isBuffItem() const;
     bool isEquipItem() const;
-    EquipSlot getEquipSlotHint() const; // 장비면 Weapon/Armor 반환, 아니면 기본값
+    EquipSlot getEquipSlotHint() const; // ?λ퉬硫?Weapon/Armor 諛섑솚, ?꾨땲硫?湲곕낯媛?
+=======
+#pragma once
+#include <string>
+
+class Character;
+
+enum class ItemType
+{
+    HealPotion,
+    AtkPotion
+};
+
+class Item
+{
+protected:
+    std::string name;
+    ItemType type;
+    int price; 
+
+public:
+    Item(const std::string& name, ItemType type, int price)
+        : name(name), type(type), price(price) {
+    }
+
+    virtual ~Item() = default; 
+
+    std::string getName() const { return name; }
+
+    int getPrice() const { return price; }
+
+    ItemType getType() const { return type; }
+
+    virtual void use(Character* character) = 0;
+};
+
+
+class HealthPotion : public Item
+{
+    int healAmount;
+
+public:
+    HealthPotion(const std::string& name, int price, int healAmount) // 체력포션
+        : Item(name, ItemType::HealPotion, price), healAmount(healAmount) {
+    }
+
+    void use(Character* character) override
+    {
+        (void)character;
+    }
+};
+
+class AttackPotion : public Item
+{
+    int atkAmount;
+
+public: 
+    AttackPotion(const std::string& name, int price, int atkAmount) // 공격물약
+        : Item(name, ItemType::AtkPotion, price), atkAmount(atkAmount) {
+    }
+
+    void use(Character* character) override
+    {
+        (void)character;
+    }
+};
+
+class LongSword : public Item
+{
+    int healAmount;
+
+public:
+     LongSword(const std::string& name, int price, int healAmount) // 체력포션
+        : Item(name, ItemType::HealPotion, price), healAmount(healAmount) {
+    }
+
+    void use(Character* character) override
+    {
+        (void)character;
+    }
+}; 
+
+class HealthPotion : public Item
+{
+    int healAmount;
+
+public:
+    HealthPotion(const std::string& name, int price, int healAmount) // 체력포션
+        : Item(name, ItemType::HealPotion, price), healAmount(healAmount) {
+    }
+
+    void use(Character* character) override
+    {
+        (void)character;
+    }
+>>>>>>> origin/feature/Shop
 };
