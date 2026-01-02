2f49f0f2281121ae932c28d09354f61a21f4de49
hyunseo <hansx2079@naver.com>
2026-01-02 14:30:34 +0900

    add: Button 기능 class 추가

--- Project1/Project1/Engine.cpp
+++ Project1/Project1/Engine.cpp
@@ -4,6 +4,10 @@
 #include "TestScene.h"
 #include "Input.h"
 #include <Windows.h>
+#include "MainMenu.h"
+
+bool Engine::exitRequested = false;
+bool Engine::isMainMenu = false;
 
 Engine::Engine(int w, int h):renderer(w, h)
 {
@@ -35,27 +39,38 @@ Renderer& Engine::GetRenderer()
     return renderer;
 }
 
-//int main() {
-//    Engine engine(160, 50);
-//
-//    SceneManager::GetInstance().Register("TestScene", [&]() {
-//        return std::make_unique<TestScene>();
-//        });
-//
-//    using clock = std::chrono::steady_clock;
-//    auto prev = clock::now();
-//
-//    SceneManager::GetInstance().LoadScene("TestScene");
-//
-//    while (engine.IsRunning())
-//    {
-//        auto now = clock::now();
-//        std::chrono::duration<float> delta = now - prev;
-//        prev = now;
-//        float dt = delta.count();
-//
-//        engine.Update(dt);
-//    }
-//
-//    return 0;
-//}
+int main() {
+    Engine engine(160, 50);
+
+    SceneManager::GetInstance().Register("TestScene", [&]() {
+        return std::make_unique<TestScene>();
+        });
+    SceneManager::GetInstance().Register("MainMenu", [&]() {
+        return std::make_unique<MainMenu>();
+        });
+
+    using clock = std::chrono::steady_clock;
+    auto prev = clock::now();
+
+    SceneManager::GetInstance().LoadScene("MainMenu");
+
+    while (engine.IsRunning())
+    {
+        auto now = clock::now();
+        std::chrono::duration<float> delta = now - prev;
+        prev = now;
+        float dt = delta.count();
+
+        engine.Update(dt);
+
+        if (Engine::exitRequested == true) {
+            break;
+        }
+    }
+
+    if (Engine::isMainMenu == true) {
+        std::cout << "Main Menu로 돌아갑니다." << std::endl;
+    }
+
+    return 0;
+}
