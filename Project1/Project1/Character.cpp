4dbbe286529a309a0f7dfe8c9d25dd708fb9b02c
juoqkdbwk1073-cmd <juoqkdbwk1073@gmail.com>
2026-01-06 19:15:19 +0900

    Character✨

--- Project1/Project1/Character.cpp
+++ Project1/Project1/Character.cpp
@@ -1,2 +1,22 @@
-#include "Character.h"
+﻿#include "Character.h"
 Character* Character::instance = nullptr;
+
+void Character::levelUp()
+{
+    // 만렙일시 레벨업 불가
+    if (level >= 10)
+    {
+        experience = 0;
+        return;
+    }
+
+    // 레벨업
+    if (experience >= 100)
+    {
+        level += 1;
+        experience = 0;
+
+        // 레벨업시 체력회복
+        health = 100;
+    }
+}
--- Project1/Project1/VariableSlime.cpp
+++ Project1/Project1/VariableSlime.cpp
@@ -1,1 +1,32 @@
-#include "VariableSlime.h"
+﻿#include "VariableSlime.h"
+
+
+//반환 함수들
+std::string VariableSlime::getName()
+{
+    return name;
+}
+
+std::string VariableSlime::getMobInfo()
+{
+    return mobInfo;
+}
+
+int VariableSlime::getHealth()
+{
+    return health;
+}
+
+int VariableSlime::getAttack()
+{
+    return attack;
+}
+
+//몬스터 데미지 처리
+void VariableSlime::takeDamage(int damage)
+{
+    health -= damage;
+
+    if (health < 0)
+        health = 0;
+}
